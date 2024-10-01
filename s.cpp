adae0a4a713144c5c92458deb4648e8ea5c12eec
ritika-80 <ritikayadav7860@gmail.com>
2024-10-01 15:34:45 +0530

    Add files via upload

---
+++ s.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+'int main()'{
+    int r;
+    float a,c;
+    'clrscr'();
+    printf("enter the radius of circle");
+    scanf("%d",&r);
+    a=3.14*r*r;
+    c=2*3.14*r;
+    printf("area=%f",a);
+    printf("circumference=%f",c);
+    getch();
+}
+
+
+    
+}
